f9f1c65d5e6cf1f269dbbe7d1505544b644f8331
Viktar <viktorsemerikov@mail.ru>
2017-10-25 22:11:06 +0300

    small changes in dialog

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
-$("#order-call-popup" ).dialog({
-	autoOpen: false,
-    modal: true,
-    show: { effect: "fadeIn", duration: 400 },
-    hide: {effect: "fadeOut", duration: 400}
+$("#order-call-popup").dialog({
+  autoOpen: false,
+  modal: true,
+  buttons: {
+    "Заказать звонок": function() {
+      $(this).dialog("close");
+    },
+    "Отмена": function() {
+      $(this).dialog("close");
+    }
+  },
+  show: {
+    effect: "fadeIn",
+    duration: 400
+  },
+  hide: {
+    effect: "fadeOut",
+    duration: 400
+  },
+  resizable: false,
+  draggable: false
 });
 
-$('#order-call-button').click(function(event){
-    $('#order-call-popup').dialog("open");
-    event.preventDefault();
+$('#order-call-button').click(function(event) {
+  $('#order-call-popup').dialog("open");
+  event.preventDefault();
 });
