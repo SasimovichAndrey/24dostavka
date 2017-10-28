d76e57ee19e65e0d3683e1882dab1abfdd589835
Andrey Sasimovich <ballack.tyta@gmail.com>
2017-10-28 15:22:48 +0300

    forms backend

--- src/backend/conf.php
+++ src/backend/conf.php
@@ -1,6 +1,8 @@
 <?php
-	$MAIL_USER_NAME = "username";
-	$MAIL_USER_PASSWORD = "password";
-	$MAIL_PORT = 587;
-	$ADMIN_MAIL_ADDRESS = "twobro.company@gmail.com"
+	class Configuration{
+		public static $MAIL_USER_NAME = "username";
+		public static $MAIL_USER_PASSWORD = "password";
+		public static $MAIL_PORT = 587;
+		public static $ADMIN_MAIL_ADDRESS = "twobro.company@gmail.com";
+	}
 ?>
