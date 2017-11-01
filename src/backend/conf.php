dc3c2e45fd4cffa896645d89f4ddca59a8e69395
Sinitsin <ballack.tyta@gmail.com>
2017-11-01 15:41:20 +0300

    fix payment image, change logo font and image

--- src/backend/conf.php
+++ src/backend/conf.php
@@ -1,7 +1,7 @@
 <?php
 	class Configuration{
-		public static $MAIL_USER_NAME = "ballack.tyta@gmail.com";
-		public static $MAIL_USER_PASSWORD = "kO4pffR`";
+		public static $MAIL_USER_NAME = "username";
+		public static $MAIL_USER_PASSWORD = "password";
 		public static $MAIL_PORT = 587;
 		public static $ADMIN_MAIL_ADDRESS = "twobro.company@gmail.com";
 	}
